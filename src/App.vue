e87534eaec91e95469514897d914b5d3e8060689
Lionel Cassar <lionelcassar2501@gmail.com>
2021-06-22 14:57:23 -0300

    refresh projet

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,35 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="home">
+      <h1>Math Quizz App</h1>
+      <OperatorQuizz v-if="operator" :operator="operator" @onBack="clearOperator"/>
+      <OperatorSelector v-if="!operator" @selectOperator="selectOperator"/>
+  </div>
   </div>
 </template>
+
+<script>
+import OperatorSelector from '@/components/OperatorSelector.vue'
+import OperatorQuizz from '@/components/OperatorQuizz.vue'
+
+export default {
+  name: 'Home',
+  components:{OperatorSelector, OperatorQuizz},
+  data(){
+    return{
+      operator:null
+    }
+  },
+  methods:{
+    selectOperator(operator){
+      this.operator = operator
+    },
+    clearOperator(){
+      this.operator = null
+    }
+  }
+}
+</script>
 
 <style>
 #app {
